5b442a17dae77b45d67cd6016370131afa703b94
VanrougeK <ochoavania795@gmail.com>
2026-04-22 16:07:56 -0600

    Modified Ranking and skins
    En ranking se hizo una prueba de usuarios antes de la db, en los skins se implementara una galeria donde hay mas skins y podran crear skins, se probo el panel de skins que son las skins disponibles del usuario

--- gato-cepillao/src/components/Ranking.jsx
+++ gato-cepillao/src/components/Ranking.jsx
@@ -1,7 +1,18 @@
 export default function Ranking ({ onVolver}) {
+    const prueba = [
+        {nombre:"pepe", puntuacion:50},
+        {nombre:"pelado", puntuacion:-50},
+        {nombre:"peladopro", puntuacion:500}
+    ]
     return (
         <div>
-            <p>Ranking</p>
+            <h1>Ranking</h1>
+            {prueba.map((jugador, index) => (
+                <div key={index}>
+                    <span>{index + 1}.{jugador.nombre}</span>
+                    <span>{jugador.puntuacion} puntos</span>
+                </div>
+            ))}
             <button onClick={onVolver}>Volver</button>
         </div>
     )
--- gato-cepillao/src/components/SkinPanel.jsx
+++ gato-cepillao/src/components/SkinPanel.jsx
@@ -1,7 +1,31 @@
-export default function SkinPanel ({ puntuacion, skinActiva, onChangeSkin, onVolver }) {
+export default function SkinPanel ({ puntuacion, skinActiva, onChangeSkin, onVolver, onGaleria, onSubir }) {
+    const skins = [
+        {id:"default", nombre:"Default", ptsReq:0},
+        {id:"naranja", nombre:"Naranja", ptsReq:0},
+        {id:"negro", nombre:"Negro", ptsReq:100},
+    ]
+    
     return (
         <div>
-            <p>Skins</p>
+            <h1>Mis Skins</h1>
+            <p>Tus puntos: {Math.floor(puntuacion)}</p>
+
+            {skins.map(skin => {
+                const desbloq = puntuacion >= skin.ptsReq
+                const activa = skinActiva === skin.id
+
+                return(
+                    <div key={skin.id}>
+                        <img src={`/${skin.id === "default" ? "gato" : `skin_${skin.id}`}_normal.png`} alt={skin.nombre} />
+                        <p>{skin.nombre}</p>
+                        <p>{desbloq ? "Desbloqueado" : `${skin.puntosRequeridos} pts`}</p>
+                        <button onClick={() => desbloq && onChangeSkin(skin.id)} disabled={!desbloq||activa}>{activa ? "Activa" : desbloq ? "Usar" : "Bloqueado"}</button>
+                    </div>
+                )
+            })}
+
+            <button onClick={onGaleria}>Mas Skins</button>
+            <button onClick={onSubir}>Subir mi skin</button>
             <button onClick={onVolver}>Volver</button>
         </div>
     )
