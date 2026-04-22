93e44f6fc23bea11c20a2d4986cbf7e9a532684e
VanrougeK <ochoavania795@gmail.com>
2026-04-21 22:55:09 -0600

    Modified gameover add score

--- gato-cepillao/src/components/GameOver.jsx
+++ gato-cepillao/src/components/GameOver.jsx
@@ -2,7 +2,7 @@ export default function GameOver ({nombre, puntuacion, onReintentar, onRanking, 
     return (
         <div>
             <p>Game Over</p>
-            <p>{nombre} -- {puntuacion} pts</p>
+            <p>{nombre} -- {Math.floor(puntuacion)} puntos</p>
             <button onClick={onReintentar}>Reintentar</button>
             <button onClick={onRanking}>Ranking</button>
             <button onClick={onSkins}>Skins</button>
