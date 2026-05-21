d3ce1d34c5e513948f79663d36665d6adcb0aafb
VanrougeK <ochoavania795@gmail.com>
2026-05-21 12:29:28 -0600

    Modified... again
    Tomado del proyecto tunna.art

--- gato-cepillao/postcss.config.js
+++ gato-cepillao/postcss.config.js
@@ -1,9 +1,5 @@
-import tailwindcss from "@tailwindcss/vite";
-import autoprefixer from "autoprefixer";
-
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
-};
+}
